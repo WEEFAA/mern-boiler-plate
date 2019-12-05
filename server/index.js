233593026e665829348201ef7e836667cc0ef81c
WEEFAA <rayranilfu7o7@gmail.com>
2019-12-05 15:52:15 +0800

    --reconstructing server setup

--- server/index.js
+++ server/index.js
@@ -5,41 +5,46 @@ const MongoDbStore = require('connect-mongodb-session')(session)
 
 // app||server instance
 const app = express()
+
 // configurations
 const config = !process.env.NODE_ENV ? require('../config') : process.env
+
 const { 
 	PORT, DEV_DB, SESSION_SECRET,
 	MONGO_DB_USER, MONGO_DB_PASSWORD, MONGO_DB_HOST,
-	MONGO_DB_NAME, MONGO_DB_PORT
+	MONGO_DB_NAME, MONGO_DB_PORT, MONGO_DB_PREFIX
  } = config 
 
 // construct database uri 
-const mongodb_uri = DEV_DB ||  `mongodb://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@${MONGO_DB_HOST}.mlab.com:${MONGO_DB_PORT}/${MONGO_DB_NAME}`
+const mongo_uri = DEV_DB || `${MONGO_DB_PREFIX}://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@${MONGO_DB_HOST}/${MONGO_DB_NAME}?${MONGO_DB_OPTIONS}`
 const db_options = {
 	useNewUrlParser:true,
 	useFindAndModify:true
 }
 // connect to the database production||development
-const database = mongoose.connect(mongodb_uri, db_options)
+const database = mongoose.connect(mongo_uri, db_options)
 
 //create a session store
-const sessionStore = new MongoDbStore({ uri: mongodb_uri })
+const sessionStore = new MongoDbStore({ uri: mongo_uri })
 
 // configuring session
 app.use(session({
 	secret: SESSION_SECRET,
-	cookie: { maxAge: 1000 * 60 * 60 * 24 * 1 /* one day*/ },
+	cookie: { 
+		maxAge: 1000 * 60 * 60 * 24 * 1 /* one day*/ 
+	},
 	resave: false,
 	saveUninitialized:false,
-	store: sessionStore
+	store: sessionStore // your session store can be seen in your database collections
 }))
 
+//test route <-- delete this route once all setup are complete
 app.get('/', function(req,res){
 	res.sendStatus(200)
 })
 
 //Listen in current port
-app.listen(PORT, () => console.log(`>SERVER RUNNING ON PORT:${PORT}`))
+app.listen(PORT, () => console.log(`SERVER RUNNING ON PORT:${PORT}`))
 
 //EVENTS
 // listen for connection
